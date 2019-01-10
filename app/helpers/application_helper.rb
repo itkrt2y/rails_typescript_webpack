75dcd8841b27138b2f546d7d06df667b282d27bc
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-01-10 14:06:17 +0900

    Add a script tag helper for bundle files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def javascript_bundle_tag(filename)
+    javascript_include_tag(webpack_manifest["#{filename}.js"], skip_pipeline: true, defer: true)
+  end
+
+  private
+
+    def webpack_manifest
+      @webpack_manifest ||= JSON.load(Rails.root.join("public", "javascripts", "dist", "webpack-manifest.json"))
+    end
 end
